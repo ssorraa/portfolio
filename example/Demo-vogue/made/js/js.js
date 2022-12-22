126fc6ae86baf91cbf93da865ceda3a26d2ab8da
ssorraa <118324510+ssorraa@users.noreply.github.com>
2022-12-22 14:05:37 +0900

    1222

--- example/Demo-vogue/made/js/js.js
+++ example/Demo-vogue/made/js/js.js
@@ -1,28 +1,43 @@
-$(document).ready(function() {
-$('nav>ul li:nth-child(1)').click(function() {
-  $('section>div').removeClass('on');
-  $('section>div:nth-child(1)').addClass('on');
-  $('#menubtn').show();
-  $('nav').removeClass('on');
-  $('section').removeClass('on');
+$(document).ready(function () {
+  $('nav>ul li:nth-child(1)').click(function () {
+    $('section>div').removeClass('on');
+    $('section>div:nth-child(1)').addClass('on');
+    $('#menubtn').show();
+    $('nav').removeClass('on');
+    $('section').removeClass('on');
+  })
+  $('nav>ul li:nth-child(2)').click(function () {
+    $('section>div').removeClass('on');
+    $('section>div:nth-child(2)').addClass('on');
+    $('#menubtn').show();
+    $('nav').removeClass('on');
+    $('section').removeClass('on');
+  })
+  $('nav>ul li:nth-child(3)').click(function () {
+    $('section>div').removeClass('on');
+    $('section>div:nth-child(3)').addClass('on');
+    $('#menubtn').show();
+    $('nav').removeClass('on');
+    $('section').removeClass('on');
+  })
+  $('#menubtn').click(function () {
+    $(this).hide();
+    $('nav').addClass('on');
+    $('section').addClass('on');
+  })
+  $('.logo').click(function () {
+    $('#menubtn').show();
+    $('nav').removeClass('on');
+    $('section').removeClass('on');
+  })
 })
-$('nav>ul li:nth-child(2)').click(function() {
-  $('section>div').removeClass('on');
-  $('section>div:nth-child(2)').addClass('on');
-  $('#menubtn').show();
-  $('nav').removeClass('on');
-  $('section').removeClass('on');
-})
-$('nav>ul li:nth-child(3)').click(function() {
-  $('section>div').removeClass('on');
-  $('section>div:nth-child(3)').addClass('on');
-  $('#menubtn').show();
-  $('nav').removeClass('on');
-  $('section').removeClass('on');
-})
-$('#menubtn').click(function() {
-  $(this).hide();
-  $('nav').addClass('on');
-  $('section').addClass('on');
-})
-})
+
+
+
+let rkor = document.getElementById('kor')
+let ko;
+
+function korset() {
+  ko = Number(prompt('국어점수는?'));
+  rkor.innerHTML = ko;
+}
