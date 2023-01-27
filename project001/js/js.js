acc1fb9da0e8e27f38adf7ade55cd48825cbfffc
ssorraa <118324510+ssorraa@users.noreply.github.com>
2023-01-27 09:22:29 +0900

    Add files via upload

--- project001/js/js.js
+++ project001/js/js.js
@@ -1,12 +1,14 @@
+  // 18:41~19:25
+  // 21:00~22:53
 $(document).ready(function () {
-  // 메인슬라이드-높이 지정
+  // 중요! 메인슬라이드-높이 지정
   let ht = $('.slide .imgBox>a>img').height()
-  console.log(ht)
   $('.slide .imgBox').height(ht)
   // 전체 스크롤 동작
   $(window).scroll(function () {
     let sc = $(this).scrollTop();
-    if (sc >= 200 && sc < 1700) {
+    let nsc = $('.news').offset().top;
+    if (sc >= nsc/4 && sc < $('.news').height()+(nsc/3)) {
       $('.news .conBox').addClass('on');
     } else {
       $('.news .conBox').removeClass('on');
@@ -17,29 +19,25 @@ $(document).ready(function () {
       $('.bg .active').removeClass('on');
     };
     if (sc >= 200) {
-      $('.topBtn').css({'display':'flex'});
-      $('.topBtn').stop().animate({'opacity':1},400);
+      $('.topBtn').addClass('on')
     } else {
-      $('.topBtn').stop().animate({'opacity':0},400,
-        function(){
-          $('.topBtn').css({'display':'none'});
-        });
+      $('.topBtn').removeClass('on');
     }
   })
   // 상단 버튼 동작
   $('.topBtn').click(function () {
     $('html,body').stop().animate({
       'scrollTop': 0
-    }, 600)
+    }, 600);
   })
   // 메인슬라이드-호버
   $('.slide .imgBox>i').mouseenter(function(){
-    let i = $(this).index()
-    $('.slide .imgBox>img').eq(i-8).addClass('on')
+    let i = $(this).index();
+    $('.slide .imgBox>img').eq(i-7).addClass('on');
   })
   $('.slide .imgBox>i').mouseleave(function(){
-    let i = $(this).index()
-    $('.slide .imgBox>img').eq(i-8).removeClass('on')
+    let i = $(this).index();
+    $('.slide .imgBox>img').eq(i-7).removeClass('on');
   })
   // 메인슬라이드-시간
   let a = 0;
@@ -58,7 +56,7 @@ $(document).ready(function () {
     $('.slide .imgBox>a>img').css({'border-radius':0});
     $('.slide .imgBox>a').css({'pointer-events':'none'});
     let i = $(this).index()
-    if(i == 8) {
+    if(i == 7) {
       $('.slide .imgBox>a').eq(a-1).css({'left':'-100%'}).animate({'left':0});
       $('.slide .imgBox>a').eq(a).css({'left':0}).animate({'left':'100%'},
       function(){
@@ -67,7 +65,7 @@ $(document).ready(function () {
       });
       a--;
       if(a < 0) {a = 3;};
-    }else if (i == 9) {
+    }else if (i == 8) {
       a++;
       if(a > 3) {a = 0;};
       $('.slide .imgBox>a').eq(a-1).css({'left':0}).animate({'left':'-100%'});
@@ -80,28 +78,33 @@ $(document).ready(function () {
   });
   // 메인 슬라이드-높이 변경
   $(window).resize(function(){
-    ht = $('.slide .imgBox>a>img').height()
-    console.log(ht)
-    $('.slide .imgBox').height(ht)
+    ht = $('.slide .imgBox>a>img').height();
+    $('.slide .imgBox').height(ht);
   })
   // 뉴스-태그목록-클릭
-  // 여기서부터
   let newsi = 0;
   $('.news .tag li').click(function(){
     if ($(this).index() >= 3) {
       alert('준비중입니다');
     } else{
       newsi = $(this).index();
-      $('.news .tag li').removeClass('on');
-      $('.news .tag li').eq(newsi).addClass('on');
-      $('.listBox .box ul').removeClass('on');
-      $('.listBox .box ul').eq(newsi).addClass('on');
-      $('.news .conBox article').removeClass('on');
-      $('.news .conBox article').eq(newsi*8).addClass('on');
+      $('.news .tag li').removeClass('on').eq(newsi).addClass('on');
+      $('.news .conBox article').removeClass('on').eq(newsi*8).addClass('on');
+      $('.listBox .box ul').removeClass('on').eq(newsi).addClass('on');
       $('.listBox .box li').css({'order':0});
       b = 0;
   };
   })
+  // 뉴스-뉴스콘텐츠-태그 클릭
+  $('.news .conBox article span').click(function(){
+    let i = $('.news .tag li').text().indexOf($(this).text());
+    $('.news .tag li').eq(i/5).trigger('click');
+  });
+  // 뉴스-뉴스목록-뉴스 클릭
+  $('.listBox .box li').click(function(){
+    let i = $(this).index();
+    $('.news .conBox article').removeClass('on').eq((newsi*8)+i).addClass('on');
+  });
   // 뉴스-뉴스목록-아이콘 클릭
   let b = 0;
   $('.listBox .left i').click(function(){
@@ -109,23 +112,28 @@ $(document).ready(function () {
       b = 8;
       $('.listBox .box ul').eq(newsi).children('li').css({'order':0});
     };
-    $('.listBox .box ul').eq(newsi).children('li').eq(b-1).css({'order':-1});
-    $('.listBox .box ul').eq(newsi).css({'left':'-250px'}).animate({'left':0},
+    $('.listBox .box ul').eq(newsi).css({'left':'-250px'}).animate({'left':0},100,
     function(){
-    });
+    }).children('li').eq(b-1).css({'order':-1});
     b--;
   });
   $('.listBox .right i').click(function(){
-    $('.listBox .box ul').eq(newsi).css({'left':0}).animate({'left':'-250px'},
+    b++;
+    $('.listBox .box ul').eq(newsi).css({'left':0}).animate({'left':'-250px'},100,
     function(){
+      $('.listBox .box ul').eq(newsi).css({'left':0}).children('li').eq(b-1).css({'order':1});
+      $('.listBox .box ul').eq(newsi).children('li').eq(b-1).css({'order':1});
+      $('.listBox .box ul').eq(newsi).css({'left':0});
       if(b >= 8) {
         b = 0;
         $('.listBox .box ul').eq(newsi).children('li').css({'order':0});
       };
-      $('.listBox .box ul').eq(newsi).children('li').eq(b-1).css({'order':1});
-      $('.listBox .box ul').eq(newsi).css({'left':0});
     });
-    b++;
   });
+  // 공지-시간
+
+  // 인포-책-클릭
+
+  // 인포-팝업-클릭
 
 });
