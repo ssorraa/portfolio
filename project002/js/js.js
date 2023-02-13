f4d93452297ff086b9c879485a0694b336d4b9ce
ssorraa <118324510+ssorraa@users.noreply.github.com>
2023-02-13 17:59:45 +0900

    0213

--- project002/js/js.js
+++ project002/js/js.js
@@ -45,4 +45,79 @@ $(document).ready(function () {
       $('#info article').eq(2).find('.videoWrap').css({'top':0})
     }
   })
+  // 지도 생성
+// 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
+var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = {
+      center: new kakao.maps.LatLng(36.38266, 129.18915), // 지도의 중심좌표
+      level: 13, // 지도의 확대 레벨
+      mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
+    };  
+
+// 지도를 생성합니다    
+var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+// 장소 검색 객체를 생성합니다
+var ps = new kakao.maps.services.Places(); 
+
+// 키워드로 장소를 검색합니다
+ps.keywordSearch('레고 스토어', placesSearchCB); 
+
+// 키워드 검색 완료 시 호출되는 콜백함수 입니다
+function placesSearchCB (data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        var bounds = new kakao.maps.LatLngBounds();
+
+        for (var i=0; i<data.length; i++) {
+            displayMarker(data[i]);    
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }       
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+    } 
+}
+// 마커 위치 확인
+var markerPos;
+// 지도에 마커를 표시하는 함수입니다
+function displayMarker(place) {
+    // 마커를 생성하고 지도에 표시합니다
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x) 
+    });
+    marker.setOpacity(0.6);
+    		// 마커에 mouseover 이벤트를 등록한다
+		kakao.maps.event.addListener(marker, 'mouseover', function() {
+      marker.setOpacity(1);
+      console.log(marker.getTitle())
+  });
+
+  // 마커에 mouseout 이벤트 등록
+  kakao.maps.event.addListener(marker, 'mouseout', function() {
+    // console.log($(this).index())
+    if (marker.getZIndex() != 1) {
+    marker.setOpacity(0.6);
+    console.log(marker.getPosition())
+    console.log(marker.getTitle())
+    }
+  });
+
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, 'click', function() {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        $('#map div div div div').css({'z-index':0})
+        infowindow.setContent('<div style="padding:10px;font-size:16px;cursor:pointer;">' + place.place_name + '</div>');
+        infowindow.open(map, marker);
+        marker.setOpacity(1);
+    });
+    kakao.maps.event.addListener(infowindow, 'click', function() {
+      alert('1')
+    });
+}
 })
