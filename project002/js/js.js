392c5d304f7a5236c6908dbfbd48d0e65472884d
ssorraa <118324510+ssorraa@users.noreply.github.com>
2023-02-15 17:20:35 +0900

    0215

--- project002/js/js.js
+++ project002/js/js.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
   let wd = $(window).width()
   let ht = $(window).height()
+  // 윈도우에 맞게 크기 조절
   let wdset = function () {
     wd = $(window).width();
     ht = $(window).height();
     $('article').css({'height': ht});
     $('#product .ani').css({'height': wd * 2}).css({'top': parseInt($('#product .ani').css('left'))});
-    console.log(parseInt($('#product .ani').css('left')));
-    window.setTimeout(function () {
       let vidWd = $('#main .vid video').width();
       let vidHt = $('#main .vid video').height();
       if (vidWd < wd) {
@@ -20,19 +19,54 @@ $(document).ready(function () {
         'margin-left': -((vidWd - wd) / 2)
       })
       // 추후 오프닝
-    }, 500)
     $('#list div').css({'height': ht / 3}).css({'width': wd / 5})
     $('#info article iframe').css({'width':(ht/9)*16}).css({'margin-left':($('#info article iframe').width()-$('#info article .videoWrap').width())/-2})
     $('#info .cover').css({'height': ht*3});
     $('#end .store .text').css({'height':2*ht}).css({'width':wd})
     $('#end .store .cover').css({'height':2*ht}).css({'width':wd})
   }
+  window.setTimeout(function () {
   wdset();
+}, 500)
+  // 윈도우 사이즈 변경시 크기 조절
   $(window).resize(function () {
     wdset();
   })
   $(window).scroll(function () {
     let sc = $(this).scrollTop();
+    // 스크롤 설정
+$('section').mousewheel(function(event,delta){
+  let i = $(this).index()
+  event.preventDefault();
+  event.stopPropagation();
+  if (i < 3 && delta > 0) {
+    $('html,body').stop().animate({'scrollTop':ht*(i-1)}, 300, function(){$('section').unbind()})
+  }
+  if (i < 3 && delta < 0) {
+    $('html,body').stop().animate({'scrollTop':ht*(i+1)}, 300, function(){$('section').unbind()})
+    if (i == 2) {
+      $(this).unbind().mousewheel();
+      console.log(1)
+    }
+  }
+  if (i == 3 && delta > 0 && sc < ht*(i-1)) {
+      $('html,body').stop().animate({'scrollTop':ht*(i-1)})
+  }
+  if (i == 3 && delta < 0 && sc > ht*(i+2)) {
+      $('html,body').stop().animate({'scrollTop':ht*(i+3)})
+  }
+  if (i > 3 && delta > 0 && $('#map').mouseenter() != true) {
+    $('html,body').stop().animate({'scrollTop':ht*(i+1)})
+    if(i == 4) {
+      $(this).unbind().mousewheel();
+      console.log(2)
+    }
+  }
+  if (i > 3 && delta < 0) {
+    $('html,body').stop().animate({'scrollTop':ht*(i+4)})
+  }
+})
+    // 동영상 flex
     if ( sc > $('#info article').eq(0).offset().top && sc < $('#info article').eq(0).offset().top+ht) {
       $('#info article').eq(0).find('.videoWrap').css({'top':sc-$('#info article').eq(0).offset().top})
     } else {
@@ -46,6 +80,7 @@ $(document).ready(function () {
     } else {
       $('#info article').eq(2).find('.videoWrap').css({'top':0})
     }
+
   })
   // 지도 생성
 // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
@@ -117,11 +152,12 @@ function displayMarker(place) {
       $('#gnb').removeClass('on')
     }
   })
+  // 제품목록 호버시 크기조절
   $('#list div').mouseenter(function(){
     if ($(this).index() != 7 ) {
-    $(this).stop().animate({'height': (ht/3)*1.15, 'width': (wd/5)*1.15, 'margin-left': -(((wd/3)*1.15)-(wd/3))/4, 'margin-top': -(((ht/5)*1.15)-(ht/5))/4}, 300).css({'z-index':5})
-    $(this).find('p').eq(0).stop().animate({'margin-left': ((wd/3*1.15)-(wd/3))/2, 'margin-top': ((ht/5*1.15)-(ht/5))/2}, 300)
-    $(this).find('p').eq(1).stop().animate({'margin-right': ((wd/3*1.15)-(wd/3))/8, 'margin-bottom': ((ht/5*1.15)-(ht/5))}, 300)
+    $(this).stop().animate({'height': (ht/3)*1.15, 'width': (wd/5)*1.15, 'margin-left': -(((wd/5)*1.15)-(wd/5))/2, 'margin-top': -(((ht/3)*1.15)-(ht/3))/2}, 300).css({'z-index':5})
+    $(this).find('p').eq(0).stop().animate({'margin-left': ((wd/5*1.15)-(wd/5))/2, 'margin-top': ((ht/3*1.15)-(ht/3))/2}, 300)
+    $(this).find('p').eq(1).stop().animate({'margin-right': ((wd/5*1.15)-(wd/5))/2, 'margin-bottom': ((ht/3*1.15)-(ht/3))/2}, 300)
     
     }
   })
@@ -133,5 +169,17 @@ function displayMarker(place) {
     $(this).find('p').eq(0).stop().animate({'margin-left': 0, 'margin-top': 0}, 200)
     $(this).find('p').eq(1).stop().animate({'margin-right': 0, 'margin-bottom': 0}, 200)
   }
+  // sns아이콘 클릭시 클래스 변경
+  })
+  $('#sns .snsIcon i').click(function(){
+    let i = $(this).index()
+    $('#sns .snsIcon i').removeClass('on')
+    $('#sns .snsImg ul').removeClass('on')
+    $(this).addClass('on')
+    $('#sns .snsImg ul').eq(i).addClass('on')
+  })
+  $('#sns .snsImg ul a').click(function(event){
+    event.preventDefault();
+    console.log('해당 사이트는 포트폴리오 사이트이기 때문에 링크로 넘어가는것을 방지합니다')
   })
 })
