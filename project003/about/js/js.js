fe38b4e3a09abadb8f3f27e7b3b9dd94ca1c6f19
ssorraa <118324510+ssorraa@users.noreply.github.com>
2023-02-23 18:18:52 +0900

    0223

--- project003/about/js/js.js
+++ project003/about/js/js.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
   let ht = $(window).height();
   let wd = $(window).width();
+  $('.bg').mouseenter(function(){
+    $('.profile').addClass('on')
+    $('.timeline').addClass('on')
+  })
+  $('.bg').mouseleave(function(){
+    $('.profile').removeClass('on')
+    $('.timeline').removeClass('on')
+  })
+  // 반복문, i번째 li에 있는 i(아이콘)가 1개 초과면 1번째꺼 투명도 낮추기
+  // for($('.timeline .list li').eq(i).length > 1) {
+  //   $('.timeline .list i').eq(0).css({'opacity':0.3})
+  //   console.log()
+  // }
   })
