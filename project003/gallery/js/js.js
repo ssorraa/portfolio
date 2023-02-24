db289c29290cd5c911ed96d8bf84b659500f6f4d
ssorraa <118324510+ssorraa@users.noreply.github.com>
2023-02-24 16:57:12 +0900

    0224

--- project003/gallery/js/js.js
+++ project003/gallery/js/js.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
   let ht = $(window).height();
   let wd = $(window).width();
+  $('.list li').click(function(){
+    $('body').removeClass()
+    if($(this).index() == 0) {
+      $('body').addClass('one')
+    } else if($(this).index() == 1) {
+      $('body').addClass('two')
+    } else {
+      $('body').addClass('three')
+    }
+  })
   })
